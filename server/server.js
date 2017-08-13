5a4a5592fc9b24a0dc82c144acb38aad076b4a19
hpatel13@calstatela.edu <hpatel13@calstatela.edu>
2017-08-13 01:02:21 -0700

    get route with specific get /todo/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -28,6 +29,21 @@ app.get('/todos',(req,res)=>{
     })
 });
 
+app.get('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    if(!ObjectID.isValid(id))
+        return res.status(404).send("Not a valid Id");
+    //res.send(req.params);
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+           return res.status(404).send("No todo found");
+        }
+        res.send({todo})
+    },(e)=>{
+        res.status(400).send(e);
+    })
+});
 
 
 app.listen(3000,()=>{
