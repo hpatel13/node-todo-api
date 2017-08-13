ce3e9cc17ae5cc2e24e92237dc2c764d99e5b749
hpatel13@calstatela.edu <hpatel13@calstatela.edu>
2017-08-13 13:34:12 -0700

    Delete test case for delete /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -89,3 +89,32 @@ describe('Get /todos/:id' , ()=> {
         request(app).get(`/todos/123`).expect(404).end(done)
     });
 });
+
+describe('Delete /todos/:id' , ()=> {
+
+    it("should remove a todo",(done)=>{
+        request(app).delete(`/todos/${todos[1]._id.toHexString()}`).expect(200).expect((res)=>{
+            //expect(res.body.todos.length).toBe(1);
+            expect(res.body.todo._id).toBe(todos[1]._id.toHexString());
+        }).end((err,res)=>{
+            if(err)
+                return done(err);
+
+            Todo.findById(`${todos[1]._id.toHexString()}`).then((todo)=>{
+                expect(todo).toNotExist();
+                done();
+            }).catch((e)=>{
+                done(e);
+            })
+
+        })
+    });
+
+    it("should return 404 if todo not found for delete",(done)=>{
+        request(app).delete(`/todos/${new ObjectID().toHexString()}`).expect(404).end(done)
+    });
+
+    it("should return 404 for non-object ids for delete",(done)=>{
+        request(app).delete(`/todos/123`).expect(404).end(done)
+    });
+});
