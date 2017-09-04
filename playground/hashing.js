1e74433879460d1d30eea4f9d74a41b4f96053d4
hpatel13@calstatela.edu <hpatel13@calstatela.edu>
2017-09-04 01:46:24 -0700

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password,salt,(err,hash)=>{
+//         console.log(hash);
+//     })
+// });
 
-var decoded = jwt.verify(token,'123abc')
-console.log("decoded: ",decoded)
+var hashedPassword = '$2a$10$CEw6iIOFWBbD/xpJwco.4ujmyFjXsrmqsQ9GlBhklBtmviz0CtlF.';
+
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);
+})
+
+// var data = {
+//     id:10
+// };
+
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'123abc')
+// console.log("decoded: ",decoded)
 // this can be done use a library jason web token, crpyto-js was used only in playground folder
 
 // var message = " I am user3";
