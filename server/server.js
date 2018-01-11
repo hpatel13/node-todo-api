f4c0319477abb87eb6ae05e15fac0188d773040f
hpatel13@calstatela.edu <hpatel13@calstatela.edu>
2018-01-10 22:47:17 -0800

    Add post /users/login

--- server/server.js
+++ server/server.js
@@ -129,6 +129,19 @@ app.get('/users/me',authenticate,function(req,res){
     // });
     res.send(req.user);
 })
+
+//Post /users/login {email,password}
+app.post('/users/login',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+
+    User.findByCredentials(body.email,body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user);
+        })
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+})
 app.listen(port,()=>{
     console.log("started server on port ",port);
 });
